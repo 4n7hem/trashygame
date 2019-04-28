06aedf7bf8ae216102995bb75d14bd14d639ddce
4n7hem <48725919+4n7hem@users.noreply.github.com>
2019-04-27 21:54:21 -0300

    everything

---
+++ Assets/Scripts/enemybehaviour.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class enemybehaviour : MonoBehaviour{
+
+    public Transform Target;
+
+    public float speed;
+    public float gravity = 9.8f;
+    public int distance;
+    public float huntDistance;
+
+    private Animator animator;
+
+    void Start (){
+        Target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update(){
+        if((Vector3.Distance(Target.position, transform.position) < huntDistance)){
+            if(Vector3.Distance(transform.position, Target.position) > distance){
+                transform.position = Vector3.MoveTowards(transform.position, Target.position, speed * Time.deltaTime);
+            }
+        }
+    }
+}
