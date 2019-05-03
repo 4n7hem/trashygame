1f164a0fe1fe6f44bc778c4692a2ecf5111ece28
4n7hem <48725919+4n7hem@users.noreply.github.com>
2019-05-03 15:09:02 -0300

    now the enemy animations work

--- Assets/Scripts/enemybehaviour.cs
+++ Assets/Scripts/enemybehaviour.cs
@@ -7,18 +7,20 @@ public class enemybehaviour : MonoBehaviour {
 
 	public Transform Target;
 	public Rigidbody2D rigidbody;
+	public Collider2D damager;
 
 	public float speed;
 	public float gravity = 9.8f;
 	public int distance;
 	public float huntDistance;
-	public bool running;
-	public bool attacking;
+	public bool _running;
+	public bool _attacking;
 	private float vx;
 	private float vy;
 	private bool facingRight;
 	private float oldPosition = 0.0f;
 	private Vector3 normalScale;
+	private Vector3 invertedScale;
 
 	private Animator animator;
 
@@ -28,38 +30,44 @@ public class enemybehaviour : MonoBehaviour {
 		rigidbody = GetComponent<Rigidbody2D>();
 		oldPosition = transform.position.x;
 		normalScale = new Vector3(transform.localScale.x, transform.localScale.y, transform.localScale.z);
+		invertedScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
 	}
 
 	void Update() {
-		
 		// how it moves
 		if ((Vector3.Distance(Target.position, transform.position) < huntDistance)) {
-			running = true;
-			if (Vector3.Distance(transform.position, Target.position) > distance) {
-				attacking = true;
-				running = false;
-				transform.position = Vector3.MoveTowards(transform.position, Target.position, speed * Time.deltaTime);
+			_running = true;
+			_attacking = false;
+			damager.enabled = false;
+			transform.position = Vector3.MoveTowards(transform.position, Target.position, speed * Time.deltaTime);
+			if (Vector3.Distance(transform.position, Target.position) <= distance) {
+				_attacking = true;
+				_running = false;
 			}
 		}
 		else{
-			running = false;
-			attacking = false;
+			_running = false;
+			_attacking = false;
 		}
 		if (transform.position.x > oldPosition) // he's looking right
-         {
+        {
 			facingRight = true;
             this.ChangeDirection();
-         }
+        }
 		if (transform.position.x < oldPosition) // he's looking left
-         {
+        {
 			facingRight = false;
             this.ChangeDirection();
-         }
+        }
          oldPosition = transform.position.x; // update the variable with the new position so we can chack against it next frame
+		 animator.SetBool("attacking", _attacking);
+		 animator.SetBool("running", _running);
 	}
+	
+	
 	public void ChangeDirection(){
 		if (facingRight == false){
-			transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+			transform.localScale = invertedScale;
 		}
 		if (facingRight == true){
 			transform.localScale = normalScale;
