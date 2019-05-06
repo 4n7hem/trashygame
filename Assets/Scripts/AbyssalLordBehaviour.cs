df37d9aac53ffe2e199cda38cf62dbf09d6088d7
4n7hem <48725919+4n7hem@users.noreply.github.com>
2019-05-06 11:14:54 -0300

    boss behaviour updated and boss arena created

--- Assets/Scripts/AbyssalLordBehaviour.cs
+++ Assets/Scripts/AbyssalLordBehaviour.cs
@@ -10,10 +10,16 @@ public class AbyssalLordBehaviour : MonoBehaviour{
 
 	public float speed;
 	public float gravity = 9.8f;
+	public float attackTimer = 0;
+	public float attackCooldown = 1.33f;
+	public float magicTimer = 0;
+	public float magicCooldown = 7.0f;
 	public int distance;
 	public float huntDistance;
 	public bool _running;
 	public bool _attacking;
+	public bool _casting;
+	private bool magicAble;
 	private float vx;
 	private float vy;
 	private bool facingRight;
@@ -36,24 +42,62 @@ public class AbyssalLordBehaviour : MonoBehaviour{
 	}
 
 	void Update() {
-        
+        //if it should move
         if(movable == false){
             _running = false;
             if(_attacking == false){
-                _attacking = true;
+                _attacking = true;				
             }
             if (Vector3.Distance(transform.position, Target.position) > distance) {
                 movable = true;                
             }
         }
+		//how it waits for a cast
+		if(magicAble){
+			if(magicTimer <= 0){
+				magicTimer = magicCooldown;
+			}
+			else if (magicTimer > 0){
+				magicTimer -= Time.deltaTime;
+				if(magicTimer <= attackCooldown){
+					_casting = true;
+				}
+				else{
+					_casting = false;
+				}
+			}
+		}
+		//how it casts magic
+		if(_casting == true){
+			int selection = Random.Range(1,3);
+			movable = false;	
+		}
+		//how the hitbox waits between each attack
+		if(_attacking == true){
+			if(attackTimer <= 0){
+				attackTimer = attackCooldown;
+			}
+			else if (attackTimer > 0){
+				attackTimer -= Time.deltaTime;
+				if(attackTimer <= attackCooldown - 0.66f && attackTimer > attackCooldown - 1.2f){
+					damager.enabled = true;					
+				}
+				else {
+					damager.enabled = false;
+				}												
+			}			
+		}
+		//how it moves
         if ((Vector3.Distance(Target.position, transform.position) < huntDistance) && movable == true) {
 			_running = true;			
 			damager.enabled = false;
             _attacking = false;
-            transform.position = Vector3.MoveTowards(transform.position, Target.position, speed * Time.deltaTime);
+			magicAble = true;
+            transform.position = Vector3.MoveTowards(transform.position, Target.position, speed * Time.deltaTime);			
 			if (Vector3.Distance(transform.position, Target.position) <= distance) {
 				_running = false;
-                movable = false;      
+                movable = false;
+				magicAble = false;      
 			}
 		}		
 		if (transform.position.x > oldPosition) // he's looking right
@@ -66,6 +110,7 @@ public class AbyssalLordBehaviour : MonoBehaviour{
 			facingRight = false;
             this.ChangeDirection();
         }
+		//just in case he's still, so it doesn't keep changing sides
         if(transform.position.x == oldPosition){
             if(facingRight == true){
                 transform.localScale = normalScale;
@@ -77,9 +122,10 @@ public class AbyssalLordBehaviour : MonoBehaviour{
          oldPosition = transform.position.x; // update the variable with the new position so we can chack against it next frame
 		 animator.SetBool("attacking", _attacking);
 		 animator.SetBool("running", _running);
+		 animator.SetBool("casting", _casting);
 	}
 	
-	
+	//how it changes the sides
 	public void ChangeDirection(){
 		if (facingRight == false){
 			transform.localScale = invertedScale;
---
+++ Assets/Scripts/loadBossOnCollision.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class loadBossOnCollision : MonoBehaviour
+{
+    public CameraSeguePersonagem cameraS;
+    public Camera camera;
+    public Transform cameraT;
+    public Transform cameraArray;
+    public GameObject boss;
+    public GameObject wall1;
+    public GameObject wall2;
+    // Start is called before the first frame update
+    void Start()
+    {        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other){
+        cameraS.enabled = false;
+        cameraT.transform.position = cameraArray.position;
+        wall1.active = true;
+        wall2.active = true;
+        boss.active = true;
+
+    }
+}
